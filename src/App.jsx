888dd6cb03821c483574b0ead51af6c30888219d
shaadali-tech <215ics043@gbu.ac.in>
2026-04-04 23:11:19 +0530

    add reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,14 @@
-import { useState } from "react";
+import { act, useReducer, useState } from "react";
+import reducer from "./Reducers/taskreducer";
 
 import "./App.css";
 import TaskForm from "./Components/TaskForm";
 
 function App() {
-  const Tasks = [
-    {
-      id: 1,
-      title: "Task 1",
-      description: "This is task 1",
-    },
-    {
-      id: 2,
-      title: "Task 2",
-      description: "This is task 2",
-    },
-    {
-      id: 3,
-      title: "Task 3",
-      description: "This is task 3",
-    },
-    {
-      id: 4,
-      title: "Task 4",
-      description: "This is task 4",
-    },
-    {
-      id: 5,
-      title: "Task 5",
-      description: "This is task 5",
-    },
-  ];
+  const Tasks = [];
 
-  const [tasks, setTasks] = useState(Tasks);
+  const [tasks, dispatch] = useReducer(reducer, Tasks);
   const [filter, setFilter] = useState("all");
-
-  const addTask = (newTask) => {
-    const taskWithId = {
-      id: Date.now(),
-      ...newTask,
-      completed: false,
-    };
-
-    setTasks((prev) => [...prev, taskWithId]);
-  };
-
-  const deleteTask = (id) => {
-    setTasks((prev) => prev.filter((task) => task.id !== id));
-  };
-
-  const toggleTask = (taskId) => {
-    setTasks((prev) =>
-      prev.map((task) =>
-        task.id === taskId ? { ...task, completed: !task.completed } : task,
-      ),
-    );
-  };
 
   const filteredTasks = tasks.filter((task) => {
     if (filter === "completed") return task.completed;
@@ -63,6 +16,16 @@ function App() {
     return true;
   });
 
+  const addTask = (newTask) => {
+    dispatch({
+      type: "ADD_TASK",
+      payload: {
+        ...newTask,
+        id: Date.now(),
+        completed: false,
+      },
+    });
+  };
   return (
     <>
       <TaskForm onTaskSubmit={addTask} />
@@ -73,8 +36,14 @@ function App() {
         <div key={task.id}>
           <h2>{task.title}</h2>
           <p>{task.description}</p>
-          <button onClick={() => deleteTask(task.id)}>Delete</button>
-          <button onClick={() => toggleTask(task.id)}>
+          <button
+            onClick={() => dispatch({ type: "DELETE_TASK", payload: task.id })}
+          >
+            Delete
+          </button>
+          <button
+            onClick={() => dispatch({ type: "TOGGLE_TASK", payload: task.id })}
+          >
             {task.completed ? "Mark as Incomplete" : "Mark as Complete"}
           </button>
         </div>
---
+++ src/Reducers/taskreducer.js
@@ -0,0 +1,22 @@
+function reducer(state, action) {
+  if (action.type === "ADD_TASK") {
+    return [...state, action.payload];
+  }
+
+  if (action.type === "DELETE_TASK") {
+    return state.filter((task) => task.id !== action.payload);
+  }
+
+  if (action.type === "TOGGLE_TASK") {
+    return state.map((task) => {
+      if (task.id === action.payload) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+  }
+
+  return state;
+}
+
+export default reducer;
